0f18fff5f276a47b0349ae15c7bd51b7fa5b5a7d
Saurav <pinkunpadhy1998@gmail.com>
2025-02-05 15:41:01 +0530

    Test Cases written & Tested

--- app.py
+++ app.py
@@ -21,11 +21,12 @@ m = folium.Map(location=[34.223334, -82.461707],
                attr='Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under ODbL',
                zoom_start=8)
 def circle_maker(x):
-    folium.Circle(location=[x[0],x[1]],
-                 radius=float(x[2])*10,
+    folium.Circle(location=[x.iloc[0], x.iloc[1]],
+                 radius=float(x.iloc[2])*10,
                  color="red",
-                 popup='{}\n confirmed cases:{}'.format(x[3],x[2])).add_to(m)
-corona_df[['Lat','Long_','Confirmed','Combined_Key']].apply(lambda x:circle_maker(x),axis=1)
+                 popup='{}\n confirmed cases:{}'.format(x.iloc[3], x.iloc[2])).add_to(m)
+
+corona_df[['Lat','Long_','Confirmed','Combined_Key']].apply(lambda x: circle_maker(x), axis=1)
 html_map=m._repr_html_()
 
 from flask import Flask,render_template
